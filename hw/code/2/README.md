2f6a9a24f296c2b6dec4252d729def94333bc46c
Timothy Goodrich <tdgoodrich@gmail.com>
2015-09-05 11:31:40 -0400

    Finished CODE2

---
+++ hw/code/2/README.md
@@ -0,0 +1,33 @@
+### Solution
+
+## Exercises for Chapter 3:
+
+Exercise 1
+
+<img src="exercise1.png">
+
+Exercise 2
+
+<img src="exercise2.png">
+
+Exercise 3
+
+<img src="exercise3.png">
+
+Exercise 4
+
+<img src="exercise4.png">
+
+Exercise 5
+
+<img src="exercise5.png">
+
+## Exercises for Chapter 4:
+
+Exercise 2
+
+<img src="think4_2.png">
+
+Exercise 3
+
+<img src="think4_3.png">
--- hw/code/2/think4.py
+++ hw/code/2/think4.py
@@ -1,17 +1,11 @@
 from swampy.TurtleWorld import *
 from math import pi, sin
 
-print "hello world"
-
+# Early examples:
 def square(t, length):
 	for i in range(4):
 		fd(t, length)
 		lt(t)	
-
-def polygon(t, length, n, angle=360.0):
-	for i in range(n):
-		fd(t, length)
-		lt(t, angle/n)
 
 def circle(t, r):
 	n = 100
@@ -20,16 +14,23 @@ def circle(t, r):
 	print length, circumference
 	polygon(t, length, n)
 
+def polygon(t, length, n, angle=360.0):
+	for i in range(n):
+		fd(t, length)
+		lt(t, angle/n)
+
 def arc(t, r, angle):
 	n = 100
 	circumference = 2*r*pi
 	length = circumference / float(n)
 	polygon(t, length, n, angle=angle)
 
-
+# Create the objects we need to run
+world = TurtleWorld()
+bob = Turtle()
+bob.delay = 0.01
 
 # Exercise 2
-
 def petal(t, r, angle):
 	for i in xrange(2):
 		arc(t, r, angle)
@@ -39,7 +40,15 @@ def flower(t, num_petals, r, angle):
     for i in range(num_petals):
         petal(t, r, angle)
         lt(t, 360.0/num_petals)
+
+# Uncomment to run:
+# flower(bob, 15, 25, 70.0)
+
 # Exercise 3
+def polygon(t, length, n, angle=360.0):
+	for i in range(n):
+		fd(t, length)
+		lt(t, angle/n)
 
 def pie(t, length, n):
 	polygon(t, length, n)
@@ -55,20 +64,7 @@ def pie(t, length, n):
 		fd(t, radius)
 		lt(t, turn_angle)
 
-
-
-
-
-world = TurtleWorld()
-bob = Turtle()
-bob.delay = 0.01
-print bob
-
-#square(bob, 100)
-#polygon(bob, 100, 6)
-#circle(bob, 50)
-#arc(bob, 25, 70.0)
-#pie(bob, 140,5)
-flower(bob, 15, 25, 70.0)
+# Uncomment to run:
+# pie(bob, 100,5)
 
 wait_for_user()
